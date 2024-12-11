b66c6b5baadd31810e0b4688610d0d5f88ba7db8
Cayden Hunsaker <caydenh@goldsystems.com>
2024-12-11 15:03:48 -0700

    updated copilot keybind

--- lua/thomriddle/packer.lua
+++ lua/thomriddle/packer.lua
@@ -85,6 +85,8 @@ return require('packer').startup(function(use)
     use {
         'github/copilot.vim',
         config = function()
+            vim.g.copilot_no_tab_map = true -- Disable default <Tab> behavior
+            vim.api.nvim_set_keymap("i", "<C-y>", 'copilot#Accept("<CR>")', { expr = true, silent = true })
         end
     }
 end)
